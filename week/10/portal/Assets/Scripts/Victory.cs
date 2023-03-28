09bf735e0ff6de49da295abfb9e4e1fbad039fdc
NotRllyRn <PinBDev@gmail.com>
2023-03-27 23:41:20 -0700

    jeez dude thats a lot

---
+++ week/10/portal/Assets/Scripts/Victory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Victory : MonoBehaviour
+{
+    public GameObject Text;
+    private Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent = Text.GetComponent<Text>();
+
+        textComponent.color = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            textComponent.color = new Color(0, 0, 0, 1);
+        }
+    }
+}

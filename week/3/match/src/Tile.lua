47f51dd9ecc672c0ea5238a34de3fd98d8687be2
NotRllyRn <PinBDev@gmail.com>
2023-04-30 21:40:32 -0700

    shiny

--- week/3/match/src/Tile.lua
+++ week/3/match/src/Tile.lua
@@ -33,12 +33,19 @@ end
 function Tile:render(x, y)
     
     -- draw shadow
-    love.graphics.setColor(34, 32, 52, 255)
+    love.graphics.setColor(34/255, 32/255, 52/255, 255/255)
     love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
         self.x + x + 2, self.y + y + 2)
 
     -- draw tile itself
-    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.setColor(1, 1, 1, 1)
     love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
         self.x + x, self.y + y)
+
+    if self.shiny then
+        -- draw shinyness :))))))))
+
+        love.graphics.setColor(1, 1, 1, 0.3)
+        love.graphics.rectangle('fill', self.x + x + 2, self.y + y + 2, 32, 32)
+    end
 end
